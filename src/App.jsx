346750b9891e84599ad4c7affeac67199a069b62
Grant Kirill <webdotg.inbox@gmail.com>
2023-11-24 07:09:20 +0300

    rote add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.module.scss'
+import style from './App.module.scss'
+import { Routes, Route } from 'react-router-dom'
+import { Header } from './components/header/header.jsx'
+import HomePage from './pages/home/homePage.jsx'
+import RegistrationPage from './pages/registration/registrationPage.jsx'
+import LoginPage from './pages/login/loginPage.jsx'
+import AddPostPage from './pages/addPost/addPostPage.jsx'
+import FullPostPage from './pages/fullPost/fullPostPage.jsx'
 
 function App() {
 
   return (
-    <>
-  <h1>test</h1>
-    </>
+    <div className={style.root}>
+      <Header />
+      <Routes>
+        <Route path="/FullStackUI" element={<HomePage />} />
+        <Route path="/FullStackUI/register" element={<RegistrationPage />} />
+        <Route path="/FullStackUI/login" element={<LoginPage />} />
+        <Route path="/FullStackUI/add-post" element={<AddPostPage />} />
+        <Route path="/FullStackUI/:id" element={<FullPostPage />} />
+      </Routes>
+    </div>
   )
 }
 
